82af8d05abf8bc35072a034584892d0a4a4e58b4
Matheus Webler <matheus.webler@gmail.com>
2017-07-08 14:21:33 -0300

    new version

--- thinkific-api.js
+++ thinkific-api.js
@@ -1,19 +1,26 @@
-const ThinkificApi = require('thinkific-api');
+//const ThinkificApi = require('thinkific-api');
 
 module.exports = function(RED) {
     function ThinkificApi(config) {
         RED.nodes.createNode(this,config);
-        this.configNode = config.configNode;
+        // Retrieve the config node
+        this.thinkific = RED.nodes.getNode(config.thinkific);
+        
         this.resource = config.resource;
         this.operation = config.operation;
         var node = this;
-        var thinkific = new ThinkificApi(config.configNode.apikey, config.configNode.subdomain);
+        //var thinkific = new ThinkificApi(config.configNode.apikey, config.configNode.subdomain);
         node.on('input', function(msg) {
-            node.send(node.resource + ':' + node.operation);
+            var r = thinkific[node.resource];
+            node.send({payload: {
+                resource: node.resource,
+                operation: node.operation,
+                thinkific: node.thinkific || 'naotem'
+            }});
         });
     }
 
-    RED.nodes.registerType("thinkific", function ThinkificApiConfig(config){
+    RED.nodes.registerType("thinkific", function (config){
         RED.nodes.createNode(this,config);
         this.apikey = config.apikey;
         this.subdomain = config.subdomain;
